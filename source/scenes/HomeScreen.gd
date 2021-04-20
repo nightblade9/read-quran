3f4125b8fd820ad7a4af53e9302364c502f16bcb
nightblade9 <nightbladecodes@gmail.com>
2021-04-19 23:25:09 -0400

    Clicking on go button changes scene to surah scene

--- source/scenes/HomeScreen.gd
+++ source/scenes/HomeScreen.gd
@@ -1,15 +1,16 @@
 extends Node2D
 
+const ShowSurahScene = preload("res://scenes/ShowSurahScene.tscn")
+
 onready var _page_number_text = $PageNumberText
 
 func _on_GoPageButton_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.pressed:
 		var page_number = _page_number_text.text.strip_edges()
+		
 		# UGH, godot returns 0 if the parsed value isn't an int
-		if page_number == "0":
-			page_number = 0 # legit page 0
-			print("Goto page ZERO")
-		else:
-			page_number = int(page_number)
-			if page_number > 0:
-				print("Goto page %s" % page_number)
+		page_number = int(page_number)
+		if page_number > 0:
+			var show_surah_scene = ShowSurahScene.instance()
+			show_surah_scene.setup(page_number)
+			get_tree().change_scene_to(show_surah_scene)
---
+++ source/scenes/ShowSurahScene.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+# Called before ready
+func setup(page_number:int) -> void:
+	pass # Look up surah based on page number
