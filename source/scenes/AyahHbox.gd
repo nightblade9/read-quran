8acdd55de23ea7cc5981350432f37e0928581af3
nightblade9 <nightbladecodes@gmail.com>
2021-04-21 23:25:53 -0400

    FINALLY got ayah box sizes right; trick was min_size.

--- source/scenes/AyahHbox.gd
+++ source/scenes/AyahHbox.gd
@@ -1,6 +1,12 @@
 extends Control
 
+onready var _background = $ColorRect
+onready var _arabic_label = $TemplateHBox/ArabicLabel
+
 func _ready():
-	$ColorRect.color.r = randf() * 0.60
-	$ColorRect.color.g = randf() * 0.70
-	$ColorRect.color.b = randf() * 0.75
+	_background.color.r = randf() * 0.40
+	_background.color.g = randf() * 0.50
+	_background.color.b = randf() * 0.65
+
+	# Expand to fit contents; +N so tashkeel (kasra) don't get cut off
+	self.rect_min_size.y = _arabic_label.get_minimum_size().y + 20
