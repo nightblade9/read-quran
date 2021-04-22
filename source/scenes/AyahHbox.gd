7ae36f5fe725b21186b6957c06d9b001d285207d
nightblade9 <nightbladecodes@gmail.com>
2021-04-21 23:09:37 -0400

    Show the surah name - doesn't quite work. Appears fine, but numbering is weirdly off.

--- source/scenes/AyahHbox.gd
+++ source/scenes/AyahHbox.gd
@@ -1,6 +1,6 @@
 extends Control
 
 func _ready():
-	$ColorRect.color.r = randf() * 0.25
-	$ColorRect.color.g = randf() * 0.5
+	$ColorRect.color.r = randf() * 0.60
+	$ColorRect.color.g = randf() * 0.70
 	$ColorRect.color.b = randf() * 0.75
--- source/scenes/ShowQuranScene.gd
+++ source/scenes/ShowQuranScene.gd
@@ -3,28 +3,32 @@ extends Control
 const ALabel = preload("res://addons/arabic-text/ALabel.gd")
 
 const _PAGES_DATA = "res://data/pages.json"
+const _SURAH_DATA = "res://data/surahs.json"
 onready var _template = $Template
 onready var _ayaat_container = $ScrollContainer/VBoxContainer
 
 var _page:Array = [] # Page data, list of ayaat
+var _surah_names:Array = []
 
 func _ready():
+	_parse_surah_names()
+	
 	remove_child(_template)
 	_template.visible = false
 	
 	for ayah in _page:
-		
-		var hbox = _template.duplicate()
-		hbox.visible = true
-		
-		var number_label =   hbox.get_node("TemplateHBox/NumberLabel")
-		number_label.text = "(%s)" % ayah["ayah_number"]
-		
-		var arabic_label = _template.get_node("TemplateHBox/ArabicLabel")
-		arabic_label.arabic_input = ayah["arabic"]
-		
+		var ayah_number = ayah["ayah_number"]
+		if ayah_number == 1:
+			# Push surah name
+			var surah_data = _surah_names[ayah["surah_number"] - 1] # base 1 to base 0
+			var surah_name = surah_data["titleAr"]
+			var hbox = _create_entry(0, surah_name)
+			_ayaat_container.add_child(hbox)
+			
+		var hbox = _create_entry(ayah_number, ayah["arabic"])
 		_ayaat_container.add_child(hbox)
-	
+		#break # uncommment this to see jankiness
+		
 	_template.queue_free()
 	_ayaat_container.remove_child(_ayaat_container.get_child(0))
 
@@ -45,6 +49,31 @@ func setup_surah(surah_number:int) -> void:
 func setup_page(page_number:int) -> void:
 	var json_data = _parse_data()
 	_page = json_data[page_number]
+	
+func _create_entry(ayah_number:int, arabic_text:String) -> HBoxContainer:
+	var hbox = _template.duplicate()
+	hbox.visible = true
+	
+	var number_label =  hbox.get_node("TemplateHBox/NumberLabel")
+	if ayah_number > 0: # ayah
+		number_label.text = "(%s)" % ayah_number
+	else:
+		number_label.queue_free() # surah heading
+		
+	var arabic_label = _template.get_node("TemplateHBox/ArabicLabel")
+	arabic_label.arabic_input = arabic_text
+	
+	print("box %s: %s" % [ayah_number, arabic_text])
+	return hbox
+
+func _parse_surah_names():
+	# TODO: singleton I guess
+	var f = File.new()
+	f.open(_SURAH_DATA, File.READ)
+	var surah_data = f.get_as_text()
+	var json_data = JSON.parse(surah_data).result # TODO: error checking?
+	f.close()
+	_surah_names = json_data
 
 func _parse_data():
 	# TODO: singleton I guess
