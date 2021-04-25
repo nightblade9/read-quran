b82598dc71a3bc1b56cf89d5f46c978583b23563
nightblade9 <nightbladecodes@gmail.com>
2021-04-24 23:35:23 -0400

    Fix: ayaat are truncated bug Ready was resizing based on the default template text (ba)

--- source/scenes/AyahHbox.gd
+++ source/scenes/AyahHbox.gd
@@ -14,13 +14,13 @@ func _ready():
 		_background.color.g = intensity + 0.15
 		_background.color.b = intensity + 0.30
 
-	# Expand to fit contents; +N so tashkeel (kasra) don't get cut off
-	self.rect_min_size.y = _arabic_label.get_minimum_size().y + 20
-
 # Called after _ready
 func setup(ayah_number:int, arabic_text:String) -> void:
 	_number_label.text = "(%s)" % ayah_number
 	_arabic_label.arabic_input = arabic_text
+
+	# Expand to fit contents; +N so tashkeel (kasra) don't get cut off
+	self.rect_min_size.y = _arabic_label.get_minimum_size().y + 20
 	
 func show_header():
 	_number_label.queue_free()
